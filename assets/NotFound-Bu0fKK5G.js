4cdb02dff3c3dd30d3efe9af96f67aec19432d25
Imam Maulana <imammaul2405@gmal.com>
2024-04-17 22:34:46 +0700

    Updates

---
+++ assets/NotFound-Bu0fKK5G.js
@@ -0,0 +1,1 @@
+import{j as e,L as r}from"./index-BhIpL2Zy.js";import{H as t,a as i}from"./index.esm-B3fTCthF.js";function o(){return e.jsxs(e.Fragment,{children:[e.jsx(t,{children:e.jsx(i,{children:e.jsx("title",{children:"Not Found!"})})}),e.jsxs("div",{children:[e.jsx("div",{children:"Not Found!!"}),e.jsx(r,{to:"/",children:"Back"})]})]})}export{o as default};
