dc984675ce701b8e40204cfc3445af3071ee15b6
Imam Maulana <imammaul2405@gmal.com>
2024-04-17 16:29:33 +0700

    Initiate project web app portfolio version 1

--- src/Components/Button.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from '@mantine/core';
-
-export default function MyButton() {
-  return <Button variant="outline" className='hidden'>Button</Button>;
-}
--- src/Components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from "@mantine/core"
-
-export default function Loading() {
-  return (
-    <div className="min-w-full min-h-screen flex justify-center items-center">
-      <Loader color="gray" size={100} />
-    </div>
-  )
-}
