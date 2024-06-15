3222bae503a708b7221e16ba472ac83d40eacf1f
Imam Maulana <imammaul2405@gmal.com>
2024-06-15 22:39:38 +0700

    Edit info job experience

--- src/Datas/experiences.ts
+++ src/Datas/experiences.ts
@@ -7,7 +7,7 @@ interface Data {
 }
 
 export const Experiences: Data[] = [
-  { title: "Systems Engineer", company: "Sysdata Solusi Digital", type: "Fulltime", start: "06/2024", end: "Present" },
+  { title: "Systems Engineer", company: "PT Sysdata Solusi Digital", type: "Fulltime", start: "06/2024", end: "Present" },
   { title: "Web Developer", company: "", type: "Freelance", start: "03/2023", end: "Present" },
   { title: "IT Staff", company: "PT BRI Cirebon Kartini", type: "Intern", start: "06/2022", end: "09/2022" },
   { title: "Aplication Admin", company: "Rumah Digital Majalengka", type: "Freelance", start: "04/2022", end: "08/2022" },
